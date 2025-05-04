2bc36393e2d075fa5121f1d097ab670f1e091f00
Rohan Gautam <rohan1gautam@gmail.com>
2025-05-04 14:28:28 +0800

    initial code dump

---
+++ src/fsm_jax.py
@@ -0,0 +1,88 @@
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+
+@partial(jax.jit, static_argnames=["iterations"])
+def fast_sweep_2d(grid, fixed_cells, obstacle, dh, iterations=5):
+    f = 1.0  # interface speed, 1 for sdf
+    large_val = 1e3
+    # epsilon = 1e-10
+    nx, ny = grid.shape
+    sweep_dirs = [
+        (0, nx, 1, 0, ny, 1),  # Top-left to bottom-right
+        (nx - 1, -1, -1, 0, ny, 1),  # Top-right to bottom-left
+        (nx - 1, -1, -1, ny - 1, -1, -1),  # Bottom-right to top-left
+        (0, nx, 1, ny - 1, -1, -1),  # Bottom-left to top-right
+    ]
+    frozen = jnp.logical_or(fixed_cells, obstacle)
+    padded = jnp.pad(grid, pad_width=1, mode="constant", constant_values=large_val)
+
+    def run_sweep(sweep_dir, grid):
+        x_start, x_end, x_step, y_start, y_end, y_step = sweep_dir
+
+        def y_loop_body(iy, grid):
+            def x_loop_body(ix, grid):
+                piy, pix = iy + 1, ix + 1
+                a = jnp.minimum(grid[piy, pix - 1], grid[piy, pix + 1])
+                b = jnp.minimum(grid[piy - 1, pix], grid[piy + 1, pix])
+                updated_val = jnp.where(
+                    frozen[iy, ix],
+                    grid[piy, pix],  # no change if frozen
+                    jnp.minimum(  # min of curr and updated val
+                        grid[piy, pix],
+                        jnp.where(  # eqn 2.4
+                            jnp.abs(a - b) >= f * dh,
+                            jnp.minimum(a, b) + f * dh,
+                            (a + b + jnp.sqrt(2 * (f * dh) ** 2 - (a - b) ** 2)) / 2,
+                        ),
+                    ),
+                )
+                return grid.at[piy, pix].set(updated_val)
+
+            x_indices = jnp.arange(x_start, x_end, x_step)
+            return jax.lax.fori_loop(
+                0,
+                len(x_indices),
+                # ix is 0..len(x_indices) - we need to map it to actual range
+                lambda ix, grid: x_loop_body(x_indices[ix], grid),
+                grid,
+            )
+
+        y_indices = jnp.arange(y_start, y_end, y_step)
+        return jax.lax.fori_loop(
+            0,
+            len(y_indices),
+            lambda iy, grid: y_loop_body(y_indices[iy], grid),
+            grid,
+        )
+
+    def iteration_body(_, cur_grid):
+        # perform 4 sweeps (2 dimentions)
+        grid_s1 = run_sweep(sweep_dirs[0], cur_grid)
+        grid_s2 = run_sweep(sweep_dirs[1], grid_s1)
+        grid_s3 = run_sweep(sweep_dirs[2], grid_s2)
+        grid_s4 = run_sweep(sweep_dirs[3], grid_s3)
+        return grid_s4
+
+    final_grid = jax.lax.fori_loop(0, iterations, iteration_body, padded)
+    return final_grid[1:-1, 1:-1]
+
+
+# # dh is the grid spacing
+# start = time.time()
+# out = fast_sweep_2d(dist_grid_np, interface_mask, obstacle_mask, dh=dx, iterations=5)
+# print(f"Fast sweep took {time.time() - start}s")
+
+# start = time.time()
+# out = fast_sweep_2d(dist_grid_np, interface_mask, obstacle_mask, dh=dx, iterations=5)
+# print(f"Fast sweep took {time.time() - start}s")
+
+# out = np.array(out)
+# out[obstacle_mask.astype(bool)] = np.nan
+
+# plt.pcolormesh(X_np, Y_np, out)
+# plt.colorbar()
+# plt.contour(X_np, Y_np, out, levels=[0], colors="red")
+# plt.contour(X_np, Y_np, out, levels=[0, 0.1, 0.2, 0.3])
+# plt.gca().set_aspect(1)
---
+++ src/fsm_numpy.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def fast_sweep_2d(grid, fixed_cells, obstacle, dh, iterations=5):
+    f = 1.0  # interface speed, 1 for sdf
+    large_val = 1e3
+    # epsilon = 1e-10
+    nx, ny = grid.shape
+    sweep_dirs = [
+        (0, nx, 1, 0, ny, 1),  # Top-left to bottom-right
+        (nx - 1, -1, -1, 0, ny, 1),  # Top-right to bottom-left
+        (nx - 1, -1, -1, ny - 1, -1, -1),  # Bottom-right to top-left
+        (0, nx, 1, ny - 1, -1, -1),  # Bottom-left to top-right
+    ]
+
+    padded = np.pad(grid, pad_width=1, mode="constant", constant_values=large_val)
+
+    for _ in range(iterations):
+        for x_start, x_end, x_step, y_start, y_end, y_step in sweep_dirs:
+            for iy in range(y_start, y_end, y_step):
+                for ix in range(x_start, x_end, x_step):
+                    # dont do anything for fixed cells (interface) or obstacles
+                    if fixed_cells[iy, ix] or obstacle[iy, ix]:
+                        continue
+                    # calculate a,b from eqn 2.3 of Zhao et.al
+                    py, px = iy + 1, ix + 1
+                    # since it's a padded array and boundary+1 is a large value,
+                    # it will choose the interior value at the end, acting like one sided difference.
+                    a = np.min((padded[py, px - 1], padded[py, px + 1]))
+                    b = np.min((padded[py - 1, px], padded[py + 1, px]))
+                    # explicit unique solution to eq 2.3, given by eq 2.4
+                    xbar = (
+                        large_val  # xbar will be the distance to this cell from front
+                    )
+                    if np.abs(a - b) >= f * dh:
+                        xbar = np.min((a, b)) + f * dh
+                    else:
+                        # can add small eps to sqrt later for stability
+                        xbar = (a + b + np.sqrt(2 * (f * dh) ** 2 - (a - b) ** 2)) / 2
+                    # update if new distance is smaller
+                    padded[py, px] = np.min((padded[py, px], xbar))
+    # return un-padded array
+    return padded[1:-1, 1:-1]
+
+
+# # dh is the grid spacing
+# start = time.time()
+# out = fast_sweep_2d(dist_grid_np, interface_mask, obstacle_mask, dh=dx, iterations=5)
+# print(f"Fast sweep took {time.time() - start}s")
+# out[obstacle_mask.astype(bool)] = np.nan
+
+# plt.pcolormesh(X_np, Y_np, out)
+# plt.colorbar()
+# plt.contour(X_np, Y_np, out, levels=[0], colors="red")
+# plt.contour(X_np, Y_np, out, levels=[0, 0.1, 0.2, 0.3])
+# plt.gca().set_aspect(1)
